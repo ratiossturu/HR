8b26a4a3396d6640e7efce80af26a5ab91102221
rati sturua <sturua1010@gmail.com>
2019-03-29 00:14:41 +0400

    change

--- HR/Controllers/SampleDataController.cs
+++ HR/Controllers/SampleDataController.cs
@@ -11,7 +11,7 @@ namespace HR.Controllers
     {
         private static string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "rati", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         [HttpGet("[action]")]
